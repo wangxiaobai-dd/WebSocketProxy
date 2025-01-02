b48f293e6dad6b4cf98bcf6ca3c4800c8c8b8456
wangxiaobai-dd <leoeee@icloud.com>
2025-01-02 22:47:53 +0800

    errgroup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ZTWssProxy
 
 go 1.22
+
+require golang.org/x/sync v0.10.0
--- run/main.go
+++ run/main.go
@@ -1,1 +1,11 @@
-package run
+package main
+
+import "golang.org/x/sync/errgroup"
+
+func main() {
+	var eg errgroup.Group
+
+	if err := eg.Wait(); err != nil {
+
+	}
+}
